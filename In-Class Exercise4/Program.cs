ece276e05c0c760b3a27e656a475557235a52e59
Iretiayomide <iretioluwa@usf.edu>
2020-02-07 12:26:59 -0500

    New

---
+++ In-Class Exercise4/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace In_Class_Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+namespace In_Class_Exercise4b
+{
+    // Multiple objects in a class
+    class Car
+    {
+        public string color = "red";
+        static void Main(string[] args)
+        {
+            Car myObj1 = new Car();
+            Car myObj2 = new Car();
+            Console.WriteLine(myObj1.color);
+            Console.WriteLine(myObj2.color);
+        }
+    }
+    // Access objects of another class
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+    }
+}
+namespace In_Class_Exercise4c
+{
+    // Class with fields
+    class MyCar
+    {
+        string color = "red";
+        int maxSpeed = 200;
+
+        static void Main(string[] args)
+        {
+            MyCar myObj = new MyCar();
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.maxSpeed);
+        }
+    }
+}
+namespace In_Class_Exercise4d
+{
+    // multiple objects of the same class
+    class Car
+    {
+        string model;
+        string color;
+        int year;
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car();
+            Ford.model = "Mustang";
+            Ford.color = "red";
+            Ford.year = 1969;
+
+            Car Opel = new Car();
+            Opel.model = "Astra";
+            Opel.color = "white";
+            Opel.year = 2005;
+
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.model);
+        }
+    }
+}
+    namespace In_Class_Exercise4e
+    {
+    // calling a method
+    class Car
+    {
+        string color;                 // field
+        int maxSpeed;                 // field
+        public void fullThrottle()    // method
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            myObj.fullThrottle();  // Call the method
+        }
+    }
+}
+
+
+
+
+
+
+    
+
